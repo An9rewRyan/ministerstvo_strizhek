2a9f7d4c4bcabbd7382595aa061efc4a3a9e313d
Ерёменко Семён <deutschman1999@mail.ru>
2022-02-08 14:17:00 +0300

    Check css

--- core/urls.py
+++ core/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('offline/', TemplateView.as_view(template_name="offline.html")),
     path("", include("pwa.urls")),
-] 
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
